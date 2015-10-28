7bfc1ba415ee170fd34df6328c044763fdb584d9
Parshva Shah <g7parsh@gmail.com>
2015-10-27 20:40:09 -0400

    Hope There's room for this

---
+++ Room.py
@@ -0,0 +1,41 @@
+'''
+name of room
+
+room.n bool hasDoor if yes
+room.s
+room.w
+room.e
+
+
+
+Conections to other rooms
+list of people in this room
+list of objects in this room
+
+Map Class:
+	List of rooms
+	gen all rooms
+	Base it on JSON
+	
+'''
+
+from Entity import *
+from Player import *
+
+class Room(object):
+	"""name of room
+		Conections to other rooms, will be None is there isn't one
+		list of people in this room
+		list of objects in this room"""
+	def __init__(self):
+		self.players = None
+		self.north= None
+		self.south= None
+		self.east= None
+		self.west= None
+		self.objects = None
+		self.ID = None
+		self.name = "A Room"
+ 		
+
+
