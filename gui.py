ff0eb8ec525f434dace063d3d3270c97ae3fc1e5
Phrawger <phrawger@gmail.com>
2015-11-01 21:14:52 -0500

    GUI Update
    This is much better.

--- gui.py
+++ gui.py
@@ -1,113 +1,146 @@
 from Tkinter import *
 import threading
 import tweepy
+import os.path
 from webbrowser import open_new_tab
 
+#Open an error file for debugging.
 sys.stderr = open("errorfile.txt","w")
-#Stored information:
-#[0] = consumer_token
-#[1] = consumer_secret
-#Add this later, once authentication is working
 
+#userArray stores the list of players in the current instance of the game.
+userArray = []
 
+#consumerToken and consumerSecret are used for Twitter Authentication. They are unique to our app and should not be changed.
+consumerToken = "NZMk6RWDNI8d41GbTcZI4eWQf"
+consumerSecret = "FQVENANJPofLA2JBifQZiMn04nTU0yDhxzwdJuTyG312mbenuJ"
 
-userarray = []
+#authentication is a part of the Twitter API. It is the gatekeeper between our app and the user account.
+authentication = tweepy.OAuthHandler(consumerToken, consumerSecret)
 
-consumer_token = "NZMk6RWDNI8d41GbTcZI4eWQf"
-consumer_secret = "FQVENANJPofLA2JBifQZiMn04nTU0yDhxzwdJuTyG312mbenuJ"
+#authCheck is used to determine if the program has authenticated a user account before. 
+authCheck = os.path.isfile("previous.auth")
 
-auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
+#accessKey and accessSecret are the user side pieces of Twitter Authentication. authName is used to hold the name of the authenticated user.
+accessKey = ""
+accessSecret = ""
+authName = "None"
 
-api = tweepy.API(auth)
+if authCheck:
+    #A file containing previous authentication information exists.
+    #We now try to read in previous.auth and its stored user access information.
+    with open("previous.auth") as f:
+        #Read in all lines from the auth file.
+        authParams = [x.strip('\n') for x in f.readlines()]
+    accessKey = authParams[0]
+    accessSecret = authParams[1]
+    #Load the authentication parameters into the API authentication.
+    authentication.set_access_token(accessKey, accessSecret)
 
-#from user, get the following:
-#auth.access_token
-#auth.access_token_secret
+#theAPI is our API object from tweepy. It takes in as much authentication information as we've accumulated at this time.
+theAPI = tweepy.API(authentication)
 
-def put(lb, e):
-    if e.get() != "" and e.get() not in userarray:
-        lb.insert(END, e.get())
-        userarray.append(e.get())
+#If our user is authenticated already, then we can load 
+try:
+    authName = theAPI.me().name
+except tweepy.TweepError:
+    authName = "None"
+
+def AddUser(listBox, entryBox):
+    '''This function adds the user name in entryBox to the userArray. It is also listed in the listBox.'''
+    if entryBox.get() != "" and entryBox.get() not in userArray:
+        listBox.insert(END, entryBox.get())
+        userArray.append(entryBox.get())
         
-    e.delete(0,END)
-    print userarray
+    entryBox.delete(0,END)
+    print userArray
     
-def rementry(lb):
-    if userarray != []:
-        userarray.remove(lb.get(ANCHOR))
-    lb.delete(ANCHOR)
-    
-    print userarray
+def RemoveUser(listBox):
+    '''This function removes the user name from the listBox, and from the userArray.'''
+    if userArray != []:
+        userArray.remove(listBox.get(ANCHOR))
+    listBox.delete(ANCHOR)
+    print userArray
 
-def getAccessToken(name,b,ac):
+def GetAccessToken(accessCode):
+    '''This function opens the page to get an access code from the host client's Twitter account. It also opens the entry box to input the access code.'''
     try:
-        redirect_url = auth.get_authorization_url()
+        redirectURL = authentication.get_authorization_url()
     except tweepy.TweepError:
-        print 'Error! Failed to get request token.'
+        print "Error! Failed to get request token."
         return
-    open_new_tab(redirect_url)
-    ac.configure(state=NORMAL)
-    ac.delete(0,END)
+    open_new_tab(redirectURL)
+    accessCode.configure(state=NORMAL)
+    accessCode.delete(0,END)
     
-def confirmAccessToken(ac,b):
+def ConfirmAccessToken(accessCode,botRunner):
+    '''This function attempts to authenticate the access code provided in the accessCode entry box. If successful, it sets the botRunner variable to the newly authenticated account.'''
     try:
-        auth.get_access_token(ac.get())
+        token = authentication.get_access_token(accessCode.get())
     except tweepy.TweepError:
-        print 'Error! Failed to get access token.'
+        print "Error! Failed to get access token."
         return
-    b.set(api.me().name)
-    ac.configure(state=DISABLED)
+    botRunner.set(theAPI.me().name)
+    accessCode.configure(state=DISABLED)
+    with open("previous.auth","w+") as file:
+        file.write(token[0]+'\n')
+        file.write(token[1]+'\n')
 
 class App(threading.Thread):
-
+    '''This is the central GUI class for the host client.'''
     def __init__(self):
         threading.Thread.__init__(self)
         self.start()
 
     def callback(self):
-        self.root.quit()
+        self.rootWindow.quit()
     
     def run(self):
-        self.root = Tk()
-        self.root.protocol("WM_DELETE_WINDOW", self.callback)
-        self.botrunner =StringVar()
-        self.botrunner.set("None")
+        #Create the root window. This is where all of the other GUI elements will be placed. It's unresizable at the moment, and the close button breaks the main loop.
+        self.rootWindow = Tk()
+        self.rootWindow.resizable(width=FALSE, height=FALSE)
+        self.rootWindow.protocol("WM_DELETE_WINDOW", self.callback)
         
-        self.frame00 = Frame(self.root, height=1, bd=1, relief=SUNKEN)
-        self.frame01 = Frame(self.root, height=1, bd=1, relief=SUNKEN)
-        self.frame10 = Frame(self.root, height=1, bd=1, relief=SUNKEN)
-        self.frame11 = Frame(self.root, height=1, bd=1, relief=SUNKEN)
+        #botRunner is a string variable that holds the name of the currently authenticated user.
+        self.botRunner=StringVar()
+        self.botRunner.set(authName)
         
+        #The main window is broken up into 4 frames.
+        #00 holds the host client name [IDname], the label prompt for that name [IDprompt], and the button to change the host client account [IDrequest].
+        #10 holds the entry field for the access code [accessCode] and the button to validate the access code [validate].
+        #01 holds the entry field to add a new player [nameEntry] and the button to confirm that player [enterButton].
+        #11 holds the list box of currently added users [userListBox] and the button to remove the currently selected user from the list box [remove].
+        self.frame00 = Frame(self.rootWindow, height=1, bd=1, relief=SUNKEN)
+        self.frame01 = Frame(self.rootWindow, height=1, bd=1, relief=SUNKEN)
+        self.frame10 = Frame(self.rootWindow, height=1, bd=1, relief=SUNKEN)
+        self.frame11 = Frame(self.rootWindow, height=1, bd=1, relief=SUNKEN)
+        
+        #The frames are then added to the main window here. The names are based on their grid coordinates.
         self.frame00.grid(row=0, column=0)
         self.frame01.grid(row=0, column=1)
         self.frame10.grid(row=1, column=0)
         self.frame11.grid(row=1, column=1)
         
-        
-        
         accessCode = Entry(self.frame10, exportselection = 0,state=DISABLED)
         accessCode.pack()
-        validate = Button(self.frame10, text = "Validate", command = lambda ac=accessCode, b = self.botrunner: confirmAccessToken(ac,b) ).pack()
+        
+        validate = Button(self.frame10, text = "Validate", command = lambda ac=accessCode, b = self.botRunner: ConfirmAccessToken(ac,b) ).pack()
         
         IDprompt = Label(self.frame00, text = "Current ID:").pack()
-        IDname = Label(self.frame00, textvariable = self.botrunner).pack()
-        IDrequest = Button(self.frame00, text = "Set Output Account", command = lambda n=IDname, b = self.botrunner, ac = accessCode: getAccessToken(n,b,ac)).pack()
+        IDname = Label(self.frame00, textvariable = self.botRunner).pack()
+        IDrequest = Button(self.frame00, text = "Set Output Account", command = lambda ac = accessCode: GetAccessToken(ac)).pack()
         
-        nameentry = Entry(self.frame01, exportselection = 0)
-        nameentry.pack()
+        nameEntry = Entry(self.frame01, exportselection = 0)
+        nameEntry.pack()
         
-        userlist = Listbox(self.frame11)
+        userListBox = Listbox(self.frame11)
         
-        enterbutton = Button(self.frame01, text = "Add User", command = lambda lb=userlist, e=nameentry: put(lb,e)).pack()
+        enterButton = Button(self.frame01, text = "Add User", command = lambda listBox=userListBox, e=nameEntry: AddUser(listBox,e)).pack()
         
-        userlist.pack()
+        userListBox.pack()
         
-        remove = Button(self.frame11, text = "Remove User", command = lambda lb=userlist: rementry(lb) ).pack()
+        remove = Button(self.frame11, text = "Remove User", command = lambda listBox=userListBox: RemoveUser(listBox) ).pack()
         
-
-        
-        self.root.mainloop()
+        self.rootWindow.mainloop()
 
 
 app = App()
-print('Now we can continue running code while mainloop runs!')
