6890a59bc8e50bcef8f155612beb0fbb2a5d71b2
Parshva Shah <g7parsh@gmail.com>
2015-11-04 16:36:49 -0500

    updated room

--- Room.py
+++ Room.py
@@ -36,7 +36,6 @@ class Room(Entity):
 		self.west= None
 		self.objects = None
 		self.ID = '00'
-		self.name = "A Room"
  
 
 	def x(self):
@@ -45,6 +44,7 @@ class Room(Entity):
 		return int(self.ID[1])
 
 roomList = []
+linklist = []
 
 
 
@@ -54,25 +54,32 @@ for x in range(0,6):
 	for y in range(0,6):
 		temp = Room()
 		temp.ID = str(x)+str(y)
+		temp.name = "Room " +temp.ID
+		linklist.append(temp)
 		templist.append(temp)
 	roomList.append(templist)
 q = 0
-#for i in roomList:
-#	for j in roomList:
-#		if i !=j:
-#			if int(i.ID) == (int(j.ID) - 1):
-#				i.east = j
-#				j.west = i
-#				#print "E/W Made"
-#			if int(i.ID) == (int(j.ID) - 10):
-#				i.south = j
-#				j.north = i
-#				#print "N/S Made"
+for i in linklist:
+	for j in linklist:
+		if i !=j:
+			if int(i.ID) == (int(j.ID) - 1):
+				i.east = j
+				j.west = i
+				#print "E/W Made"
+			if int(i.ID) == (int(j.ID) - 10):
+				i.south = j
+				j.north = i
+				#print "N/S Made"
 
 
 for x in range(0,6):
 	for y in range(0,6):
-		print str(roomList[x][y].x()) + str(roomList[x][y].y()),
+		#print str(roomList[x][y].x()) + str(roomList[x][y].y()),
+		pass
+	#print
+
+
+for item in roomList:
+	for item2 in item:
+		print item2.name,
 	print
-
-
