4c7019654a3c2507c62e616a60ba52f132d49eb7
Parshva Shah <g7parsh@gmail.com>
2015-11-05 17:44:11 -0500

    More comments on stuff i did.

--- Map.py
+++ Map.py
@@ -6,9 +6,11 @@ class Map(object):
 		super(Map,self).__init__()
 		Map.rooms = []
 		Map.name = "Test"
-					# file pointer to a .json
+					# file pointer to a .json used for testing 
 	def GetJSON(self,JSON):
+		'''Get a dict from a .json, mostly used to debug right now. '''
 		return json.load(JSON)
+	# converts the list of room objects to a json for later use.
 	def EncodeMap(self):
 		Map.jsonFile = open("resources\\Map.json",'w')
 		Map.dictForm = {}
@@ -17,6 +19,7 @@ class Map(object):
 		for x in Map.rooms:
 			Map.dictForm['rooms'].append(x.Encode())
 		json.dump(Map.dictForm,Map.jsonFile,indent = -1)
+		#does the opposite of the above.
 	def DecodeJSON(self,_dict):
 		for room in _dict['rooms']:
 			Map.rooms.append(Room(_dict = room))
@@ -25,26 +28,8 @@ class Map(object):
 
 M = Map()
 
-roomList = []
-linklist = []
 mapfile = open("resources\\Map.json",'r')
-#mapJson = json.load(mapfile)
-#A = Room(1)
-#B=Room(2)
-#C=Room(3)
-#
-#A.north = "02"
-#A.south = "03"
-#
-#B.south = "01"
-#C.north = "01"
-#
 mapJson = M.GetJSON(mapfile)
-#M.rooms.append(A)
-#M.rooms.append(B)
-#M.rooms.append(C)
-
-#M.EncodeMap()
 
 M.DecodeJSON(mapJson)
 for x in M.rooms:
