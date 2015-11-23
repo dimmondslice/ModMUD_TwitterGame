ac62f0f2b5f95452b6f8539d7c2c7da1d19e99ea
rpietrzak258 <richpietrzak64@gmail.com>
2015-11-23 17:39:35 -0500

    fixed bug with Tweet command

--- Tweet.py
+++ Tweet.py
@@ -9,22 +9,22 @@ class Tweet(Command):
 
     def Parse(self, words, _dm, player):
         response = ""
-        if len(words) == 1:            
+        if len(words) == 1:
             self.twit.SendPic(_dm[0], _dm[2])
-            self.twit.SendMessage(_dm[2], _dm[1], "You just tweeted the last 4 messages", _dm[0])
+            response = "You just tweeted the last 4 messages"
         #if message is of the form " tweet 6" tweet the last 6 messages so long as "6" is a number
         elif len(words) == 2:
-            #test to ensure that words[1] is an int    
+            #test to ensure that words[1] is an int
             try:
                 int(words[1])
             except ValueError:
                 self.twit.SendMessage(_dm[2], _dm[1], words[1] + " is not a number", _dm[0])
                 return
-                
+
             response = "You just tweeted the last "+words[1]+ " messages"
             #tweet out a hightlights pictuer that displays the last  words[1] number of messages between the player and the bot
             self.twit.SendPic(_dm[0], _dm[2], int(words[1]))
-            
+
         #if more then 2 words are entered, send an error
         elif len(words) > 2:
             response = 'Improper use of "tweet" command. Try "Tweet" or "Tweet X" where X is a number'
