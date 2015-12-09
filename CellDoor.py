41d99724a6641c34ba7143bfada684fc2f4382a2
Spencer <johnss18@rpi.edu>
2015-12-09 01:36:48 -0500

    fucking tabs

--- CellDoor.py
+++ CellDoor.py
@@ -2,50 +2,50 @@ from Actor import Actor
 from Map import Map
 
 class CellDoor(Actor):
-	"""docstring for CellDoor"""
-	def __init__(self, _dict = None):
-		if _dict != None:
+    """docstring for CellDoor"""
+    def __init__(self, _dict = None):
+        if _dict != None:
             self.Decode(_dict)
         else:
-			super(CellDoor, self).__init__()
-			self.name = "unnamed cell door"
-			#of the form ["direction door leads", ID of room the door leads to]
-			self.adjacentRoom = None	
+            super(CellDoor, self).__init__()
+            self.name = "unnamed cell door"
+            #of the form ["direction door leads", ID of room the door leads to]
+            self.adjacentRoom = None    
 
-	#cell door's use function will unlock the door if the player uses the right key on it, then it will reinstate the connection between the room
-	#that contains this door, and the adjacent room
-	def Use(self, _actorUsedWith):
-		if(_actorUsedWith.name == "cell " + self.location.ID + " key"):
-			#now re enable the neighbor connections
-			self.location.neighbors[self.adjacentRoom[0]] = self.adjacentRoom[1] 
-			neighborRoom = Map.GetRoombyID(self.adjacentRoom[1])
-			#to to the neighbor room and re enable its connection to this room
-			neighborRoom.neighbors[self.OppositeDir(_dict["adjacentRoom"])] = self.location.ID
+    #cell door's use function will unlock the door if the player uses the right key on it, then it will reinstate the connection between the room
+    #that contains this door, and the adjacent room
+    def Use(self, _actorUsedWith):
+        if(_actorUsedWith.name == "cell " + self.location.ID + " key"):
+            #now re enable the neighbor connections
+            self.location.neighbors[self.adjacentRoom[0]] = self.adjacentRoom[1] 
+            neighborRoom = Map.GetRoombyID(self.adjacentRoom[1])
+            #to to the neighbor room and re enable its connection to this room
+            neighborRoom.neighbors[self.OppositeDir(_dict["adjacentRoom"])] = self.location.ID
 
-	def Decode(self, _dict):
-		super(CellDoor, self).Decode(self, _dict)
+    def Decode(self, _dict):
+        super(CellDoor, self).Decode(self, _dict)
 
-		#now is when I will actually set self.adjecentRoom, bc I can garauntee I know self.location now
+        #now is when I will actually set self.adjecentRoom, bc I can garauntee I know self.location now
 
-		roomID = self.location.neighbors[ _dict["adjecentRoom"]]
-		#within the code,unlike the JSON, self.adjacentRoom is actually a list in the form ["direction this door leads to", id of that room]
-		self.adjacentRoom = [ _dict["adjacentRoom"], roomID]
-		#now disable the neighbor connections
-		neighborID = self.location.neighbors[_dict["adjacentRoom"]]
-		neighborRoom = Map.GetRoombyID(neighborID)
-		#gross thing then just returns the opposite direction of the one given is used to turn off the "other side of this door"
-		neighborRoom.neighbors[self.OppositeDir(_dict["adjacentRoom"])] = 0
-		neighborID = 0	#have this rooms neighbor point to nothing
+        roomID = self.location.neighbors[ _dict["adjecentRoom"]]
+        #within the code,unlike the JSON, self.adjacentRoom is actually a list in the form ["direction this door leads to", id of that room]
+        self.adjacentRoom = [ _dict["adjacentRoom"], roomID]
+        #now disable the neighbor connections
+        neighborID = self.location.neighbors[_dict["adjacentRoom"]]
+        neighborRoom = Map.GetRoombyID(neighborID)
+        #gross thing then just returns the opposite direction of the one given is used to turn off the "other side of this door"
+        neighborRoom.neighbors[self.OppositeDir(_dict["adjacentRoom"])] = 0
+        neighborID = 0  #have this rooms neighbor point to nothing
 
-	def OppositeDir(self, _dir):
-		_dir = _dir.lower()
-		if _dir == "north":
-			return "south"
-		elif _dir == "south":
-			return "north"
-		elif _dir == "west":
-			return "east"
-		elif _dir == "east":
-			return "west"
-		
-		
+    def OppositeDir(self, _dir):
+        _dir = _dir.lower()
+        if _dir == "north":
+            return "south"
+        elif _dir == "south":
+            return "north"
+        elif _dir == "west":
+            return "east"
+        elif _dir == "east":
+            return "west"
+        
+        
